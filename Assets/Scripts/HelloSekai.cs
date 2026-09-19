dd9769c45591851c0373ca4a5753f7a0ed4a49eb
Volkyn Dioux <thegreediest@gmail.com>
2026-09-19 00:41:21 -0500

    This is the second commit of HelloSekai.cs

--- Assets/Scripts/HelloSekai.cs
+++ Assets/Scripts/HelloSekai.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class HelloSekai : MonoBehaviour
 {
-	// Start is called before the first frame update
 	void Start()
 	{
 		Debug.Log("Hello Sekai");
 	}
 	
-	// Update is called once per frame
 	void Update()
 	{
 	
